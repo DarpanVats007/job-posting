a14c0ad9bbfac9121fdb5c732595c33e1a51099b
Darpan Vats <dva@protechnology.de>
2024-04-22 18:59:13 +0200

    - configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,41 @@
-import { defineConfig } from "vitest/config"
-import react from "@vitejs/plugin-react"
+import { checker } from "vite-plugin-checker";
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
-    open: true,
+    port: 3000,
   },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests",
-    mockReset: true,
+  //specify the jsxFactory and jsxFragment properties to tell Vite.js how to process JSX code.
+  esbuild: {
+    jsxFactory: "React.createElement",
+    jsxFragment: "React.Fragment",
   },
-})
+  css: {
+    modules: {
+      localsConvention: "camelCaseOnly",
+    },
+  },
+  root: "./",
+  build: {
+    outDir: "./dist",
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+    }),
+    viteCompression({
+      algorithm: "gzip",
+      ext: ".gz",
+    }),
+  ],
+});
